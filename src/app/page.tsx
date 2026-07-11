64d69d1e0a16cda82d855b62db420003a3bada05
Parth <108parthnikam@gmail.com>
2026-07-12 03:03:29 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,30 @@
 export default function About() {
   return (
     <div className="flex flex-col gap-8 max-w-5xl w-full">
-      <div className="flex flex-col gap-8 md:flex-row md:items-start">
+      <div className="gap-8 md:items-start">
         <div className="space-y-4">
           <h1 className="text-xl font-bold text-foreground mb-4">Hi, I'm Parth Nikam</h1>
           <p className="leading-relaxed text-foreground">
             19 y/o living in bangalore, exploring llms and agentic ai.
-            <br />
-            Besides learning how to use these tools, I've gone the extra mile to learn how they work down to the matrix multiplication level.
+            </p>
+          <p className="leading-relaxed text-foreground">
+            i know bit of math and ton of code. i built transformer from scratch. know how multiple transformer architectures work. 
+          </p>
+
+          <p className="leading-relaxed text-foreground">
+            i forced myself to read 60 papers on llms in 48 hours and had my agent quiz me every couple of hours to test my undestanding. 
+          </p>
+
+          <p className="leading-relaxed text-foreground">
+            currently building some agentic ai stuff. 
           </p>
           <p className="leading-relaxed text-foreground">
-            Currently trying to learn how to use GPUs efficiently for running LLMS, understanding their bottlenecks while training and write cuda kernels to optimize things.
-          </p>
-          <p className="leading-relaxed text-foreground">
-            I do a bit of ML, a bit of web dev, a bit of competitive programming and a bit of meditation.
-          </p>
-          <p className="leading-relaxed text-foreground">
-            In my free time, I run a lot(sprinting), read novels and obsess about founders and companies. Everything I do stems from an intrinsic curiosity to understand how things work and how they can provide value to people.
+            i do a bit of ML, a bit of web dev, a bit of competitive programming and a bit of meditation.
           </p>
         </div>
-        <div className="max-w-[400px] shrink-0">
+         <div className="min-w-[200px] shrink-0">
           <img
-            className="block w-full rounded-xl object-cover"
+            className="block object-cover"
             src="/logo.jpg"
             width={200}
             height={200}
