c9a8a0a509307199313c12bb92f1a2056350e569
Parth <108parthnikam@gmail.com>
2026-07-12 03:09:54 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function About() {
          <div className="min-w-[200px] shrink-0">
           <img
             className="block object-cover"
-            src={"/static/images/logo.jpg"}
+            src={"https://raw.githubusercontent.com/parthnikam/ParthNikam/refs/heads/main/public/static/images/logo.jpg"}
             width={200}
             height={200}
             alt="Parth image"
